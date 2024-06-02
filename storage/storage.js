901005850899686adb56dbcd845047c0bc811743
Simana <poudelsimana22@gmail.com>
2024-06-02 09:11:29 +0545

    feat: git format added

--- storage/storage.js
+++ storage/storage.js
@@ -11,7 +11,7 @@ const storage = new CloudinaryStorage({
   cloudinary,
   params: {
     folder: "CloudinaryDemo",
-    allowedFormats: ["jpeg", "png", "jpg"],
+    allowedFormats: ["jpeg", "png", "jpg", "webp"],
   },
 });
 
